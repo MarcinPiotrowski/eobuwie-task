e297b65fa7bfe17a37acc274f95a42b50f6faa4e
Marcin <marcinm.piotrowski@gmail.com>
2021-12-11 10:06:51 +0100

    Created Vue app with example component

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HelloWorld text="Hello world!"/>
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: { HelloWorld }
+}
+</script>
+
+<style lang="scss">
+</style>
