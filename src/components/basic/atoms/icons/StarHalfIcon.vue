10e69a55c8b14ac7139778c9a6e6e2279be0ca15
Marcin <marcinm.piotrowski@gmail.com>
2021-12-12 13:38:02 +0100

    Added Rating component
    Added 3 SVG star icons
    Added unit tests
    Added story

---
+++ src/components/basic/atoms/icons/StarHalfIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <g
+    id="StarHalf"
+    transform="translate(0.763429, 0.873879)"
+    fill="none"
+    fill-rule="evenodd"
+  >
+    <polyline
+      id="Star-Copy"
+      stroke="#474747"
+      stroke-width="10"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      transform="translate(145.061386, 100.054428) scale(-1, 1) translate(-145.061386, -100.054428) "
+      points="190.236571 156.626121 134.396972 185.982735 145.061386 123.804428 99.8862017 79.7695063 162.316771 70.6978135 190.236571 14.1261208"
+    />
+    <polygon
+      id="Path"
+      stroke="currentColor"
+      stroke-width="6"
+      fill="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      points="100.236571 14.1261208 100.236571 156.626121 44.3969718 185.982735 55.0613863 123.804428 9.88620174 79.7695063 72.3167713 70.6978135"
+    />
+  </g>
+</template>>
---
+++ src/components/basic/molecules/Rating.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="eo-rating">
+    <div class="eo-stars-container">
+      <template
+        v-for="(star, index) of stars"
+      >
+        <IconBase
+          :key="index"
+          height="14"
+          width="14"
+          icon-name="Star"
+          class="eo-star-icon"
+        >
+          <StarIcon v-if="star === StarType.Full" />
+          <StarHalfIcon v-if="star === StarType.Half" />
+          <StarEmptyIcon v-if="star === StarType.Empty" />
+        </IconBase>
+      </template>
+    </div>
+    <span class="eo-rating-count">
+      {{ ratingCount }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import IconBase from '@/components/basic/atoms/icons/IconBase.vue';
+import StarIcon from '@/components/basic/atoms/icons/StarIcon.vue';
+import StarEmptyIcon from '@/components/basic/atoms/icons/StarEmptyIcon.vue';
+import StarHalfIcon from '@/components/basic/atoms/icons/StarHalfIcon.vue';
+
+export enum StarType {
+  Full,
+  Half,
+  Empty,
+}
+
+const maxStars = 5;
+
+export default Vue.extend({
+  name: 'Rating',
+  components: { StarHalfIcon, StarEmptyIcon, StarIcon, IconBase, },
+  props: {
+    rating: {
+      type: Number,
+      required: true,
+    },
+    ratingCount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      StarType,
+    };
+  },
+  computed: {
+    ratingNormalized(): number {
+      return Math.min(maxStars, Math.max(1, this.rating));
+    },
+    stars(): StarType[] {
+      const integralPart = Math.floor(this.ratingNormalized);
+      const fractionalPart = this.ratingNormalized % 1;
+      const fullStars = integralPart + (fractionalPart >= 0.75 ? 1 : 0);
+      const halfStar = fractionalPart > 0.25 && fractionalPart < 0.75 ? 1 : 0;
+      const emptyStars = maxStars - fullStars - halfStar;
+
+      return [
+        ...new Array(fullStars).fill(StarType.Full),
+        ...new Array(halfStar).fill(StarType.Half),
+        ...new Array(emptyStars).fill(StarType.Empty),
+      ];
+    },
+  },
+}
+);
+</script>
+
+<style lang="scss" scoped>
+.eo-star-icon {
+  color: $green-color;
+}
+
+.eo-stars-container {
+  display: inline-flex;
+  justify-content: flex-end;
+  align-content: flex-end;
+}
+
+.eo-rating {
+  display: inline-flex;
+  justify-content: center;
+  align-content: center;
+}
+
+.eo-rating-count {
+  margin-left: 5px;
+  font-size: $font-tiny;
+  color: $gray-lighter-color;
+}
+</style>
